e36bf3a57da574b0b9f44f588de3945375f2a2cf
Gitisfun <christianvandenputtelaar@gmail.com>
2023-07-12 22:34:07 +0200

    Bug fix for matches

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -15,34 +15,13 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
-
-router.get("/:slug", async (req, res, next) => {
-  try {
-    const slug = req.params.slug;
-
-    if (slug) {
-      const teamInfo = await getClub({ slug });
-
-      const matches = await getAllGamesFromClub({
-        $or: [{ homeTeam: teamInfo?.name }, { awayTeam: teamInfo?.name }],
-      });
-
-      const ranking = await getRanking({ league: teamInfo?.leagueFullName });
-
-      res.send({ ...teamInfo, matches: matches, ranking });
-    } else {
-      throw new Error("No params");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
 router.get("/match", async (req, res, next) => {
   try {
     const home = req.query.home;
     const away = req.query.away;
 
+    console.log(home);
+    console.log(away);
     if (home && away) {
       const homeTeam = await getClub({ name: home });
       const awayTeam = await getClub({ name: away });
@@ -71,6 +50,28 @@ router.get("/match", async (req, res, next) => {
     }
   } catch (err) {
     next(err);
+  }
+});
+
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const slug = req.params.slug;
+
+    if (slug) {
+      const teamInfo = await getClub({ slug });
+
+      const matches = await getAllGamesFromClub({
+        $or: [{ homeTeam: teamInfo?.name }, { awayTeam: teamInfo?.name }],
+      });
+
+      const ranking = await getRanking({ league: teamInfo?.leagueFullName });
+
+      res.send({ ...teamInfo, matches: matches, ranking });
+    } else {
+      throw new Error("No params");
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
