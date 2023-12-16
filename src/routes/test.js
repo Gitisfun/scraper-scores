066d00620205688a7de26057012a8782e81c30b5
Christian <christian@wedoit.be>
2023-12-16 14:43:08 +0100

    Fixed tests

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,17 +1,12 @@
 import express from "express";
-import { fetchGamesAndDates } from "../api/games.js";
-import { fetchScores } from "../api/scores.js";
-import { scrapeAllGames } from "../jobs/index.js";
 import { runTests } from "../jobs/test.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    //const colors = [];
-    //const result = await runTests({ colors });
-    //const result = await fetchGamesAndDates()
-    const result = await scrapeAllGames()
+    const colors = [];
+    const result = await runTests({ colors });
     res.send(result);
   } catch (err) {
     console.log(err);
