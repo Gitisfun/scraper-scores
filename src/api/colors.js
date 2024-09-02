0e50bc25269b390c65445504a4ba28534d97de28
Christian vdp <christianvandenputtelaar@gmail.com>
2024-09-02 21:47:17 +0200

    Fix for color

--- src/api/colors.js
+++ src/api/colors.js
@@ -113,7 +113,7 @@ export const UNIQUE_COLOR_LIST = [
   { code: "rood/zwart", kit: Kit.MEERDERE, colors: [Colors.BLACK, Colors.RED] },
   { code: "blauw met geel", kit: Kit.MEERDERE, colors: [Colors.DARK_BLUE, Colors.YELLOW] },
   { code: "zwart/wit gestreept", kit: Kit.GESTREEPT, colors: [Colors.BLACK, Colors.WHITE] },
-  { code: "Navy Blauw/Goud", kit: Kit.MEERDERE, colors: [Colors.DARK_BLUE, Colors.GOLD] },
+  { code: "navy blauw/goud", kit: Kit.MEERDERE, colors: [Colors.DARK_BLUE, Colors.GOLD] },
   { code: "geel-rood gestreept", kit: Kit.GESTREEPT, colors: [Colors.YELLOW, Colors.RED] },
   { code: "zwart rood gestreept", kit: Kit.GESTREEPT, colors: [Colors.RED, Colors.BLACK] },
   { code: "lichtblauw", kit: Kit.ENKELVOUDIG, colors: [Colors.LIGHT_BLUE] },
